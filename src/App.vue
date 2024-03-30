6f1c595aff85ba369834234c85772b105b5cec2c
MoulOut <mmoulinlima@outlook.com>
2024-03-30 11:07:07 -0300

    feat: :sparkles: add: page content (WIp)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
     <PageHeader />
     <PageHero />
+    <PageContent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import PageHeader from './components/PageHeader.vue';
 import PageHero from '@/components/PageHero.vue';
+import PageContent from '@/components/PageContent.vue';
 
 export default defineComponent({
     name: 'App',
     components: {
         PageHeader,
         PageHero,
+        PageContent,
     },
 });
 </script>
@@ -22,12 +25,13 @@ export default defineComponent({
     --white: #fff8f2;
     --red: #f09497;
     --pink: #fdccb4;
-    --light-blue: #9ad3cf;
+    --light-blue: #d0e3dd;
     --blue: #12758d;
     --purple: #c6a8f6;
     --green: #abe5bc;
     --black: #373737;
     --background: #fcf6e8;
+    --background-gradient: linear-gradient(to bottom, #fdccb4, #d0e3dd);
     --yellow: #fec447;
 
     --primary-font: 'Poppins';
