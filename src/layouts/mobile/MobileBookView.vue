4fc3e130488a03f1d5893d2b60c26992c3d21279
Matheus Moulin Lima <59840585+MoulOut@users.noreply.github.com>
2024-04-04 16:05:49 +0000

    feat: :sparkles: (WIP) bookview layout

--- src/layouts/mobile/MobileBookView.vue
+++ src/layouts/mobile/MobileBookView.vue
@@ -2,16 +2,21 @@
     <section class="book">
         <BookstoreHead />
         <div class="book__content">
-            <img
-                :src="require(`@/assets/images/${book.title}.svg`)"
-                :alt="`Image of the book: ${book.title}`"
-                class="book-image"
-            />
-            <h3 class="book-title">{{ book.title }}</h3>
-            <p class="book-price">{{ book.price }}</p>
-            <p class="book-quantity">
-                Available in Stock:{{ book.availableStock }}
-            </p>
+            <RouterLink class="link-back" to="/bookstore">< Back</RouterLink>
+            <div class="content__info">
+                <img
+                    :src="require(`@/assets/images/${book.title}.svg`)"
+                    :alt="`Image of the book: ${book.title}`"
+                    class="book-image"
+                />
+                <div class="book__information">
+                    <h3 class="book-title">{{ book.title }}</h3>
+                    <p class="book-price">{{ book.price }}</p>
+                    <p class="book-quantity">
+                        Available in Stock:{{ book.availableStock }}
+                    </p>
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -36,5 +41,23 @@ export default defineComponent({
 .book {
     background-color: var(--background);
     padding: 2em;
+}
+
+.book__content {
+    margin-top: 1em;
+}
+
+.link-back {
+    font-weight: 500;
+    text-decoration: none;
+    color: var(--black);
+}
+
+.content-info {
+    display: flex;
+}
+
+.book-image {
+    width: 40%;
 }
 </style>
