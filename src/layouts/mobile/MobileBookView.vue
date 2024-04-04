7d1c863a0845b2c01bb946882008217c88907597
Matheus Moulin Lima <59840585+MoulOut@users.noreply.github.com>
2024-04-04 15:08:50 +0000

    feat: :sparkles: add: Mobile Book View page

---
+++ src/layouts/mobile/MobileBookView.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="book">
+        <div class="book__head">
+            <h2>MoulBooks sollections</h2>
+        </div>
+        <div class="book__content">
+            <img
+                :src="require(`@/assets/images/${book.title}.svg`)"
+                :alt="`Image of the book: ${book.title}`"
+                class="book-image"
+            />
+            <h3 class="book-title">{{ book.title }}</h3>
+            <p class="book-price">{{ book.price }}</p>
+            <p class="book-quantity">
+                Available in Stock:{{ book.availableStock }}
+            </p>
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { IBook } from '../../interfaces/book';
+
+export default defineComponent({
+    name: 'MobileBookView',
+    props: {
+        book: { type: {} as PropType<IBook>, required: true },
+    },
+});
+</script>
